5a7b4c8b9bb9f0675e842d266b53a153990c6f86
sikhabohra <95685360+sikhabohra@users.noreply.github.com>
2022-01-20 18:17:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+var getScriptPromisify = (src) => {
+  return new Promise(resolve => {
+    $.getScript(src, resolve)
+  })
+}
+
+(function () {
+  const prepared = document.createElement('template')
+  prepared.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `
+  class SamplePrepared extends HTMLElement {
+    constructor () {
+      super()
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+
+      this._root = this._shadowRoot.getElementById('root')
+
+      this._props = {}
+
+      this.render()
+    }
+
+    onCustomWidgetResize (width, height) {
+      this.render()
+    }
+
+    async render () {
+      await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+
+      const chart = echarts.init(this._root)
+const option = {
+  series: [
+    {
+      type: 'gauge',
+      axisLine: {
+        lineStyle: {
+          width: 30,
+          color: [
+            [0.3, '#67e0e3'],
+            [0.7, '#37a2da'],
+            [1, '#fd666d']
+          ]
+        }
+      },
+      pointer: {
+        itemStyle: {
+          color: 'auto'
+        }
+      },
+      axisTick: {
+        distance: -30,
+        length: 8,
+        lineStyle: {
+          color: '#fff',
+          width: 2
+        }
+      },
+      splitLine: {
+        distance: -30,
+        length: 30,
+        lineStyle: {
+          color: '#fff',
+          width: 4
+        }
+      },
+      axisLabel: {
+        color: 'auto',
+        distance: 40,
+        fontSize: 20
+      },
+      detail: {
+        valueAnimation: true,
+        formatter: '{value} %',
+        color: 'auto'
+      },
+      data: [
+        {
+          value: 70
+        }
+      ]
+    }
+  ]
+};
+setInterval(function () {
+  myChart.setOption({
+    series: [
+      {
+        data: [
+          {
+            value: 50
+          }
+        ]
+      }
+    ]
+  });
+}, 2000);
+      chart.setOption(option)
+    }
+  }
+
+  customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
+})()
